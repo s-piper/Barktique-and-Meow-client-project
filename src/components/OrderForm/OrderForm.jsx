766742d2d787224aab09836b9f31ebac3c3c1b9a
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-26 15:13:54 -0500

    Added upload button, iframe and started on check boxes

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -4,27 +4,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { FormControl } from '@material-ui/core';
-import { Notes } from '@material-ui/icons';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox'
 
 function OrderForm() {
 
     const dispatch = useDispatch();
 
-    const[order, setOrder]=useState('');
-    const[firstName, setFirstName]=useState('');
-    const[lastName, setLastName]=useState('');
-    const[phone, setPhone]=useState(0);
-    const[email, setEmail]=useState('');
-    const[image, setImage]=useState([]);
-    const[notes, setNotes]=useState('');
-    const[rights, setRights]=useState(false);
-    const[social, setSocial]=useState(false);
+    const [order, setOrder] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState(0);
+    const [email, setEmail] = useState('');
+    const [image, setImage] = useState([]);
+    const [notes, setNotes] = useState('');
+    const [rights, setRights] = useState(false);
+    const [social, setSocial] = useState(false);
 
 
 
-//Packages inputs for dispatch
+    //Packages inputs for dispatch
     const saveOrder = () => {
-        const newOrder={
+        const newOrder = {
             cus_order_number: order,
             cus_first_name: firstName,
             cus_last_name: lastName,
@@ -36,8 +37,8 @@ function OrderForm() {
             cus_social_permission: social,
         }
 
-        dispatch({type: 'ADD_ORDER', payload:newOrder})
-        
+        dispatch({ type: 'ADD_ORDER', payload: newOrder })
+
     }
 
 
@@ -76,12 +77,39 @@ function OrderForm() {
                     variant="outlined"
                     label="Email"
                     required />
+
+
+                <iframe src={image.file} height="250px" width="350px" />
+
+                <Button
+                    variant="contained"
+                    component="label">
+                    Upload Picture
+                    <input
+                        onChange={(event) => setImage({ file: URL.createObjectURL(event.target.files[0]) })}
+                        type="file"
+                        hidden />
+                </Button>
+
+
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            onChange={(event) => setRights(true)}
+                            color="primary" />
+                    }
+                    label="Yes, I own the rights to the image I am submitting and I give permission to Barktique + Meow 
+                    to use the file in order to produce the product"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            onChange={(event) => setSocial(true)}
+                            color="primary" />
+                    }
+                    label="Yes, I give permission to Barktique + Meow to use my pet photo on their social media and website"
+                />
             </FormControl>
-
-            <br></br>
-
-            
-
 
         </div>
     )
