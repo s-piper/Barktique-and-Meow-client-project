cb5a6ffb3a3386a68caf2a19ab7c75cea698cddc
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-26 14:10:10 -0500

    base admin page setup

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// function for Admin component
+function Admin() {
+    return (
+        <div>
+            <div>
+                <h2>Admin Page</h2>
+            </div>
+        </div>
+    )
+}
+
+// export Admin
+export default Admin;
