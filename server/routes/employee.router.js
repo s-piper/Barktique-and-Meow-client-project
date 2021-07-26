4c919698846cfe5bce0a6747a19dc6b5a9bfbed1
James Kelley <jkkelley86@gmail.com>
2021-07-26 11:20:49 -0500

    routes bbrought into server js file, skele routes created.

---
+++ server/routes/employee.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const axios = require('axios');
+
+
+
+
+
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const adminRouter = require('./routes/admin.router')
+const customerRouter = require('./routes/customer.router')
+const employeeRouter = require('./routes/employee.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +26,9 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/admin', adminRouter)
+app.use('/api/admin', employeeRouter);
+app.use('/api/admin', customerRouter);
 
 // Serve static files
 app.use(express.static('build'));
