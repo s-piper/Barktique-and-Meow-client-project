a79f2e8a7a6d11d8c7ff43cd8a583d8de49ecbb2
James Kelley <jkkelley86@gmail.com>
2021-07-27 09:37:27 -0500

    stashing to do PR.

--- server/routes/employee.router.js
+++ server/routes/employee.router.js
@@ -6,11 +6,27 @@ const {
 } = require('../modules/authentication-middleware');
 const axios = require('axios');
 
+// GET route Area
 
+router.get(
+  '/startOrder/v1/:employeeID',
+  rejectUnauthenticated,
+  async (req, res) => {
+    console.log(
+      `Full route and body => /api/employee/startOver/v1/:employeeID`
+    );
 
-
-
-
-
+    // Query Area
+    const updateStartOrder = `
+      UPDATE order_table SET user_id_ref=$1 and cus_order_isStarted=$2 and cus_progress_status=$3
+      ;`;
+      
+    if (req.isAuthenticated) {
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  }
+);
 
 module.exports = router;
