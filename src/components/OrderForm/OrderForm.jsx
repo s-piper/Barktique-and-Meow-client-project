58ef1a7cc10cefa71b05aeb70d3b083af99c85e4
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-28 14:47:43 -0500

    added a check for the rights and it will proceed with posting if passed

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -21,33 +21,10 @@ function OrderForm() {
     const [rights, setRights] = useState(false);
     const [social, setSocial] = useState(false);
 
-
-
-
-
-
-    //Packages inputs for dispatch
-    const saveOrder = () => {
-        const newOrder = {
-            cus_order_number: order,
-            cus_first_name: firstName,
-            cus_last_name: lastName,
-            cus_phone_number: phone,
-            cus_email: email,
-            cus_image: image,
-            cus_notes: notes,
-            cus_image_owner_rights: rights,
-            cus_social_permission: social,
-        }
-
-        dispatch({ type: 'ADD_ORDER', payload: newOrder })
-
-    }
-
     console.log('Rights', rights);
     console.log('Social', social);
 
-// Checks the checkboxes
+    // Checks the checkboxes
     const rightsCheck = () => {
         if (rights == false) {
             setRights(true);
@@ -71,20 +48,42 @@ function OrderForm() {
         var height = img.naturalHeight;
         console.log("width, height", width, height)
 
-        var area = width*height;
+        var area = width * height;
         console.log("area", area);
 
-        if(area<2_160_000){
+        if (area < 2_160_000) {
             alert("Please select a higher quality image");
 
             setImage([]);
-        } else { alert("Looks Good! Our Artists will be happy!!!")}
+        } else { alert("Looks Good! Our Artists will be happy!!!") }
     }
 
     const checkRights = () => {
-        if(rights != true) {
-           return(alert("You must own the image on your order."))
+        if (rights != true) {
+            return (alert("You must own the image on your order."))
+        } else {
+            saveOrder();
         }
+    }
+
+    //Packages inputs for dispatch
+    const saveOrder = () => {
+        const newOrder = {
+            cus_order_number: order,
+            cus_first_name: firstName,
+            cus_last_name: lastName,
+            cus_phone_number: phone,
+            cus_email: email,
+            cus_image: image,
+            cus_notes: notes,
+            cus_image_owner_rights: rights,
+            cus_social_permission: social,
+        }
+
+        console.log("newOrder", newOrder);
+
+        dispatch({ type: 'POST_CUSTOMER_ORDER_FORM', payload: newOrder });
+
     }
 
 
@@ -172,7 +171,7 @@ function OrderForm() {
                 />
 
                 <Button
-                    onClick="DO SOMETHING"
+                    onClick={checkRights}
                     variant="outlined"
                     size="large"
                     color="primary">
