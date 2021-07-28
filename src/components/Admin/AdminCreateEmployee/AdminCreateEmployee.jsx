2e93f9d7bc613e6f5e25b8d5de94446f7fb98082
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-28 09:50:26 -0500

    AdminHeader brought in

--- src/components/Admin/AdminCreateEmployee/AdminCreateEmployee.jsx
+++ src/components/Admin/AdminCreateEmployee/AdminCreateEmployee.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import AdminHeader from '../AdminHeader/AdminHeader';
 
 // AdminCreateEmployee function
 function AdminCreateEmployee() {
     
-
     return (
         <>
+            <div>
+                <AdminHeader />
+            </div>
+
+            <br />
+            
             <div>
                 <h2>Create Employee</h2>
             </div>
