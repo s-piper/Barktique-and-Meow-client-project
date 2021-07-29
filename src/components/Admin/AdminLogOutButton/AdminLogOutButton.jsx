22235b9aa2c8ff0dc324a1b4e76128f5c4e043b4
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-29 09:56:45 -0500

    Admin log out button style

--- src/components/Admin/AdminLogOutButton/AdminLogOutButton.jsx
+++ src/components/Admin/AdminLogOutButton/AdminLogOutButton.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
+// material-ui imports
+import Button from '@material-ui/core/Button';
+
 function AdminLogOutButton(props) {
 
     // set dispatch variable
     const dispatch = useDispatch();
 
     return (
-        <button
+        <Button
+            size="medium"
+            variant="contained"
+            color="primary"
             className={props.className}
             onClick={() => dispatch({ type: 'LOGOUT' })}
         >
             Admin Log Out
-        </button>
+        </Button>
     )
 } // end AdminLogOutButton
 
