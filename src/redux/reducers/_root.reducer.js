73fd0ff6bf449dc475fc102cd3d3524bab5132bc
James Kelley <jkkelley86@gmail.com>
2021-07-29 12:07:02 -0500

    GET Employee info for admin ready.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import employeeReducer from './employee.reducer';
 import {productOrderReducer} from './customerProduct.order.reducer'
+import {adminEmployeeInfoReducer} from './admin.reducer'
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   employeeReducer,
   productOrderReducer, // Reducer to handle order from customer
+  adminEmployeeInfoReducer, // Reducer to handle employee info
 });
 
 export default rootReducer;
