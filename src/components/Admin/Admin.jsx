43fd13f0b8881dfab829126eaff069170735e617
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-29 09:54:33 -0500

    create Employee button style

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -4,8 +4,23 @@ import AdminHeader from './AdminHeader/AdminHeader';
 import AdminCreateButton from './AdminCreateButton/AdminCreateButton';
 import AdminLogOutButton from './AdminLogOutButton/AdminLogOutButton';
 
+// material-ui imports
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+});
+
 // function for Admin component
 function Admin() {
+
+    const classes = useStyles();
+
     return (
         <div>
             <div>
@@ -22,6 +37,19 @@ function Admin() {
             <div>
                 <h2>Admin Page</h2>
             </div>
+            <Paper className={classes.root}>
+      <Tabs
+        // value={value}
+        // onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Orders" />
+        <Tab label="Issues" />
+        <Tab label="Artists" />
+      </Tabs>
+    </Paper>
         </div>
     )
 }
