71bd6782dfc77f16837f6a69d21aae09714ef2ca
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-30 11:02:59 -0500

    moved tabs to a new component

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,29 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Admin.css';
 
 import AdminHeader from './AdminHeader/AdminHeader';
 import AdminCreateButton from './AdminCreateButton/AdminCreateButton';
 import AdminLogOutButton from './AdminLogOutButton/AdminLogOutButton';
-// import AdminArtistTable from './AdminArtistTable/AdminArtistTable';
-
-// material-ui imports
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { AppBar } from '@material-ui/core';
-import AdminArtistTable from './AdminArtistTable/AdminArtistTable';
-
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
-});
+import AdminTabs from './AdminTabs/AdminTabs';
 
 // function for Admin component
 function Admin() {
 
-    // set material-ui classes variable
-    const classes = useStyles();
 
     return (
         <div>
@@ -41,19 +26,10 @@ function Admin() {
             <div>
                 <h2>Admin</h2>
             </div>
-            <div className={classes.root}>
-                <AppBar position="static" color="default">
-                    <Tabs
-                        indicatorColor="primary"
-                        textColor="primary"
-                        centered
-                    >
-                        <Tab label="Orders" />
-                        <Tab label="Issues" />
-                        <Tab label="Artists" />
-                    </Tabs>
-                </AppBar>
+            <div>
+                <AdminTabs />
             </div>
+            
         </div>
     )
 }
---
+++ src/components/Admin/AdminTabs/AdminTabs.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import AdminArtistTable from '../AdminArtistTable/AdminArtistTable';
+// import AdminOrdersTable from './AdminOrdersTable/AdminOrdersTable';
+// import AdminIssuesTable from '../AdminIssuesTable/AdminIssuesTable';
+
+// import material-ui
+import { makeStyles } from '@material-ui/core/styles';
+import { Tab, Tabs, AppBar } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+});
+
+function AdminTabs() {
+        // set material-ui classes variable
+        const classes = useStyles();
+
+        const [selectedTab, setSelectedTab] = useState(0);
+    
+        const handleTabs = (event, value) => {
+            console.log('Tab value:', selectedTab);
+            setSelectedTab(value);
+        } 
+
+    return (
+        <>
+            <div className={classes.root}>
+                <AppBar position="static" color="default">
+                    <Tabs
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                        value={selectedTab}
+                        onChange={handleTabs}
+                    >
+                        <Tab label="Orders" />
+                        <Tab label="Issues" />
+                        <Tab label="Artists" />
+                    </Tabs>
+                </AppBar>
+                {/* { selectedTab === 0 && <AdminOrdersTable /> }
+                { selectedTab === 1 && <AdmindIssuesTable /> } */}
+                { selectedTab === 2 && <AdminArtistTable /> }
+            </div>
+        </>
+    )
+} // end AdminTabs
+
+// export AdminTabs
+export default AdminTabs;
