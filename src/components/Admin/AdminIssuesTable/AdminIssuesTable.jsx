a6b996d0dcb0aa2600e683a867ed9a57fe4de487
terryokeefe <terryokeefe@users.noreply.github.com>
2021-08-02 09:01:15 -0500

    aws added

--- src/components/Admin/AdminIssuesTable/AdminIssuesTable.jsx
+++ src/components/Admin/AdminIssuesTable/AdminIssuesTable.jsx
@@ -37,15 +37,59 @@ function AdminIssuesTable() {
     const dispatch = useDispatch();
 
     // fetch all orders to find any with errors
+    // will need to target a new FETCH for image errors------------------
     useEffect(() => {
         dispatch({ type: 'FETCH_ALL_PRODUCT_ORDERS' })
     }, []);
 
-    const orders = useSelector(store => store)
+    // Need to target a new store for errors ------------------
+    const orders = useSelector(store => store.employee)
+
+    if (orders.cus_error_image === true) {
+        
+    }
 
     return (
         <>
-
+            <div className="issues-table">
+                {orders.map(((order, i) => {
+                    return (
+                        <div key={i} className={classes.root}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography
+                                        className={classes.heading}
+                                    >
+                                        Order # {order.cus_order_number}
+                                    </Typography>
+                                    <Typography
+                                        className={classes.heading}
+                                    >
+                                        {order.cus_first_name} {order.cus_last_name}
+                                    </Typography>
+                                    <Typography
+                                        className={classes.heading}
+                                    >
+                                        {order.cus_error_image}
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Contact Customer: {order.cus_phone_number}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        </div>
+                    )
+                }))}
+            </div>
         </>
     )
-}
+} // end AdminIssuesTable
+
+// export AdminIssuesTable
+export default AdminIssuesTable;
