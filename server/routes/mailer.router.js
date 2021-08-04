6f4a829f731f66626200b1ed15d1d0fe6a390c74
Stephen Piper <stephen.r.piper@gmail.com>
2021-08-04 10:29:05 -0500

    set up node router and start of the post

---
+++ server/routes/mailer.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+require('dotenv').config();
+
+const nodemailer = require('nodemailer')
+
+const emailUser = process.env.MAIL_USER;
+const emailPassword = process.env.MAIL_PASS;
+
+const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+        email: `${emailUser}`,
+        pass: `${emailPassword}`
+    }
+});
+
+
+router.post("/confirm", (req, res) => {
+
+    const messageOptions = {
+        from: ``,
+        to: ``,
+        subject: ``,
+        text: ``
+    };
+
+    transporter.sendMail(messageOptions, function (err, info) {
+        if (err) {
+            console.log('mailer error', err);
+            return;
+        } else {
+            console.log('Sent', info.response);
+        }
+    });
+});
